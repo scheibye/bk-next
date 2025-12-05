fc75da8a2b4a0494b8cef132c469974e2dce8598
Marichka Tanechnyk <tanechnyk.marichka07@gmail.com>
2025-12-06 01:05:52 +0200

    fix: add link to the nav header

--- src/components/landing/Header/index.tsx
+++ src/components/landing/Header/index.tsx
@@ -1,14 +1,29 @@
 import { Container } from "@/src/components/shared/Container";
 import { Logo } from "@/src/components/shared/Logo";
+import { BodyText } from "@/src/components/shared/ui/typography/BodyText";
+import { ROUTES } from "@/src/config";
+import Link from "next/link";
 
 export function Header() {
   return (
     <Container
       as="header"
       variant="inner"
-      className="p-0"
+      className="p-0 flex justify-between items-center gap-12"
     >
       <Logo className="max-w-[180px] md:max-w-none md:w-fit" />
+
+      <nav className="hidden md:block">
+        <ul>
+          <BodyText
+            as={Link}
+            href={ROUTES.BLOG_PAGE}
+            className="relative cursor-pointer after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full"
+          >
+            Blog
+          </BodyText>
+        </ul>
+      </nav>
     </Container>
   );
 }
---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  HOME_PAGE: "/",
+  BLOG_PAGE: "/blog",
+};
